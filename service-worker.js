08ea592485a6cf54fb91e515ca41b92891a1ee9d
Anton-Studen <yo.67@yahoo.com>
2026-02-06 13:45:25 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'mpfi-v1';
+
+const urlsToCache = [
+  './',
+  './mpfi.html',
+  './manifest.json'
+];
+
+// Установка (кэширование)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Очистка старого кэша при обновлении версии
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Работа офлайн (сначала ищем в кэше, потом в сети)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
